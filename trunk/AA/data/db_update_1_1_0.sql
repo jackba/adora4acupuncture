fef61b10d775e04552e217a71f90d446c4522c2a
fanqihua@gmail.com <fanqihua@gmail.com@35f3016a-7e53-11de-9318-ad35c478d30f>
2010-04-25 06:36:42 +0000

    1.1.0 alpha test ready

--- trunk/AA/data/db_update_1_1_0.sql
+++ trunk/AA/data/db_update_1_1_0.sql
@@ -1,5 +1,7 @@
 update ACUPOINT set CHINESE_NAME = replace(CHINESE_NAME, '髎', '豂') where CHINESE_NAME like '%髎%';
 update ACUPOINT set ALIAS = replace(ALIAS, '髎', '豂') where ALIAS like '%髎%';
 
+update ACUPOINT set CHINESE_NAME = '胃俞' where CHINESE_NAME = '喂俞';
+
 ------------------------------------------------------------------------------
-alert table ACUPOINT add column DESCRIPTION_ZH  text;
+alter table ACUPOINT add column DESCRIPTION_ZH  text;
